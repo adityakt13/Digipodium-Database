8c6ae8fe377ba5c17a89a11d5d22f315d923dac6
adityakt13 <adityakt13@gmail.com>
2023-01-01 15:52:32 +0530

    Initial commit

---
+++ 1. SQLite_procducts.sql
@@ -0,0 +1,7 @@
+-- SQLite
+CREATE TABLE products (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    price FLOAT,
+    description TEXT
+);
---
+++ 2. SQLite_employees.sql
@@ -0,0 +1,7 @@
+-- SQLite
+CREATE TABLE employees(
+    emp_no INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name VARCHAR(50),
+    last_ name VARCHAR(50)
+);
+INSERT INTO employee (first_name, last_name) VALUES ('John', 'Rock','Daniel', 'Jack', 'Brian', 'Punk', 'Mark', 'Henry', 'Damien', 'Bray', 'Kakarot', 'Piccolo', 'Gohan', 'Goku', 'Vegeta', 'Prince Vegeta', 'Jiren', 'Toppo', 'Frieza', 'Frost', 'Shenron', 'Broly', 'Bardock', 'Goten', 'Beerus', 'Whis', 'Champa', 'Vados', 'Zeno');
